fbc65b261e27a1a0a412b9b5b0aa888f81862d47
Oksana <ksushatarasenko@gmail.com>
2025-06-18 11:10:46 +0200

    first commit

---
+++ lekcji/części_mowy/wordLiczebniki.js
@@ -0,0 +1,320 @@
+
+const wordData = {
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "dwa": {
+        "word": "dwa",
+        "translation": "2",
+        "image": "",
+        "sentence": "Gdzie jest sala numer dwa?",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dwa.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dwa"
+    },
+    "trzy": {
+        "word": "trzy",
+        "translation": "3",
+        "image": "",
+        "sentence": "Tu są trzy komputery.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/trzy.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/trzy"
+    },
+    "cztery": {
+        "word": "cztery",
+        "translation": "4",
+        "image": "",
+        "sentence": "Mam cztery bilety do kina.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/cztery.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/cztery"
+    },
+    "pięć": {
+        "word": "pięć",
+        "translation": "5",
+        "image": "",
+        "sentence": "Tu jest pięć komputerów.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/piec.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/pięć"
+    },
+    "sześć": {
+        "word": "sześć",
+        "translation": "6",
+        "image": "",
+        "sentence": "Ola ma sześć lat.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/szesc.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/sześć"
+    },
+    "siedem": {
+        "word": "siedem",
+        "translation": "7",
+        "image": "",
+        "sentence": "To jest sala numer siedem.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/siedem.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/siedem"
+    },
+    "osiem": {
+        "word": "osiem",
+        "translation": "8",
+        "image": "",
+        "sentence": "W tej szkole jest osiem sal.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/osiem.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/osiem"
+    },
+    "dziewięć": {
+        "word": "dziewięć",
+        "translation": "9",
+        "image": "",
+        "sentence": "Czy to jest sala numer dziewięć?",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dziewiec.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dziewięć"
+    },"dziesięć": {
+        "word": "dziesięć",
+        "translation": "10",
+        "image": "",
+        "sentence": "Łucja ma dziesięć lat.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dziesiec.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dziesięć"
+    },
+// 11-20
+    "jedenaście": {
+        "word": "jedenaście",
+        "translation": "11",
+        "image": "",
+        "sentence": "W tej sali jest jedenaście krzeseł.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jedenascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jedenaście"
+    },
+    "dwanaście": {
+        "word": "dwanaście",
+        "translation": "12",
+        "image": "",
+        "sentence": "To jest numer dwanaście.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dwanascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dwanaście"
+    },
+    "trzynaście": {
+        "word": "trzynaście",
+        "translation": "13",
+        "image": "",
+        "sentence": "Mam trzynaście lat.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/trzynascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/trzynaście"
+    },
+    "czternaście": {
+        "word": "czternaście",
+        "translation": "14",
+        "image": "",
+        "sentence": "To jest mieszkanie numer czternaście.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/czternascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/czternaście"
+    },
+    "piętnaście": {
+        "word": "piętnaście",
+        "translation": "15",
+        "image": "",
+        "sentence": "Mieszkam tu piętnaście lat.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/pietnascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/piętnaście"
+    },
+    "szesnaście": {
+        "word": "szesnaście",
+        "translation": "16",
+        "image": "",
+        "sentence": "Ten budynek ma szesnaście mieszkań.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/szesnascie.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/szesnaście"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+
+
+// 21-30
+ "dwadzieścia jeden": {
+        "word": "dwadzieścia jeden",
+        "translation": "21",
+        "image": "",
+        "sentence": "",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dwadziescia_jeden.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dwadzie%C5%9Bcia+jeden"
+    },
+    "dwadzieścia dwa": {
+        "word": "dwadzieścia dwa",
+        "translation": "22",
+        "image": "",
+        "sentence": "Tam jest dwadzieścia dwa studenta.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dwadziescia_dwa.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dwadzieścia+dwa"
+    },"dwadzieścia trzy": {
+        "word": "dwadzieścia trzy",
+        "translation": "23",
+        "image": "",
+        "sentence": "",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/dwadziescia_trzy.mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/dwadzieścia+trzy"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+    "jeden": {
+        "word": "jeden",
+        "translation": "1",
+        "image": "",
+        "sentence": "Tam jest jeden student.",
+        "p1": "имя числительное",
+         "audio": "./audioSlowa1/liczebniki/jeden (1).mp3",
+        "link": "https://slovar.e-polish.eu/ru/slovo/jeden"
+    },
+
+
+    
+   
+};
+
+
+function showPopup(el) {
+  if (!el || !el.dataset || !el.dataset.id) return;
+  const id = el.dataset.id;
+  const data = wordData[id];
+  if (!data) return;
+
+  document.getElementById('popupWord').textContent = data.word;
+  document.getElementById('popupTranslation').textContent = data.translation;
+ document.getElementById('popupDetails').innerHTML = `
+  <span style="color: green;">${data.p1}</span><br> 
+`;
+  document.getElementById('popupSentence').textContent = data.sentence;
+  document.getElementById('popupImage').src = data.image;
+
+  const linkElem = document.getElementById('popupLink');
+if (data.link) {
+    linkElem.href = data.link;
+    linkElem.textContent = 'Подробнее →';
+    linkElem.style.display = 'inline';
+} else {
+    linkElem.href = '#';
+    linkElem.textContent = '';
+    linkElem.style.display = 'none';
+}
+
+  const audio = document.getElementById('popupAudio');
+  audio.pause();
+  audio.src = data.audio;
+  audio.currentTime = 0;
+  audio.onloadeddata = () => audio.play().catch(e => console.error('Аудио ошибка:', e));
+
+  document.getElementById('popupOverlay').style.display = 'flex';
+}
+
+function hidePopup() {
+  document.getElementById('popupOverlay').style.display = 'none';
+  const audio = document.getElementById('popupAudio');
+  if (audio) audio.pause();
+}
